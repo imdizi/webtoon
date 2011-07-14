290f7ac7cb967b73e8a262802fe293fd243dd8aa
imdizi <jhkgus@naver.com>
2011-07-14 04:33:42 -0700

    뒤에서 두번째 글자가 숫자인지 판별

--- webtoon.rb
+++ webtoon.rb
@@ -6,4 +6,9 @@ require 'mechanize'
 agent=Mechanize.new
 a=agent.get('http://comic.naver.com/webtoon/list.nhn?titleId=15568')
 
-puts a.search('//td[@class="title"]/a')[0].inner_text
+title=a.search('//td[@class="title"]/a')[0].inner_text
+
+if title[-3]>='0'[0] && title[-3]<='9'[0] : puts 'Sleep now'
+else puts 'Webtoon time!'
+end
+
